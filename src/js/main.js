e0493a9b97c71012e56b0834321f4980364c00f4
David-ob1 <davidvazquezt2001@gmail.com>
2024-01-11 13:29:59 -0300

    videos dinamicos con js

--- src/js/main.js
+++ src/js/main.js
@@ -18,5 +18,80 @@ moreOptions.addEventListener("click", (e) =>{
 
     moreMenu.classList.toggle("show")
 
+})
+
+
+const videos = [
+    {
+        id: "PyMlV5_HRWk",
+    },
+    {
+        id: "XCbMVbeKlCg",
+    },
+    {
+        id: "Fmdb-KmlzD8",
+    },
+    {
+        id: "lOthvD1rMbQ",
+    },
+    {
+        id: "nXDk86lQhto",
+    },
+]
+
+const sliderContainer = document.getElementById("slider")
+const currentContainer = document.getElementById("current")
+const videosContainer = document.getElementById("videos-container")
+const bNext = document.getElementById("next")
+const bPrev = document.getElementById("prev")
+let current = 0
+console.log(current)
+
+bNext.addEventListener("click" , e =>{
+
+    let changed = current
+
+    current = current + 1 < videos.length ? current + 1 : current;  
+    console.log(current)
+    
+
+    if(current != changed){
+        renderCurrentVideo(videos[current].id)
+    }
 
 })
+
+bPrev.addEventListener("click" , e =>{
+    let changed = current
+
+    current = current - 1 >= 0 ? current - 1 : current;  
+    console.log(current)
+    if(current != changed){
+        renderCurrentVideo(videos[current].id)
+    }
+
+})
+
+renderCurrentVideo(videos[current].id)
+renderVideos();
+
+function renderCurrentVideo(id){
+    currentContainer.innerHTML =`<iframe width="100%" height="560" src="https://www.youtube.com/embed/${id}" title="🔥  Sitio Web con HTML y CSS de Halo usando Flex, dark mode y responsive design" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+}
+
+function renderVideos (){
+    const html = videos.map( video =>{
+        return `
+        <div class="item">
+            <a href="#">
+                <img src="https://i3.ytimg.com/vi/${video.id}/hqdefault.jpg"
+            </a>
+        </div>
+
+        
+        `
+    })
+    videosContainer.innerHTML = html.join("")
+
+    k
+}
